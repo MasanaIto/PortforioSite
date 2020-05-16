0687af4705a71ce85a3af755a06bf7ac5cee6794
MasanaIto <z3heng0326@gmail.com>
2020-05-17 01:05:51 +0900

    Navbarのコンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/skill">Skill</router-link>
-    </div>
-    <transition mode="out-in">
-      <router-view/>
-    </transition>
+    <Navbar />
   </div>
 </template>
 <style lang="scss">
+body {
+  background: linear-gradient(#eadeb9, #e2a872);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,39 +16,12 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-.v-enter {
-  transform: translate(-100px, 0);
-  opacity: 0;
-}
-.v-enter-to {
-  opacity: 1;
-}
-.v-enter-active {
-  transition: all 1s 0s ease;
-}
-.v-leave {
-  transform: translate(0, 0);
-  opacity: 1;
-}
-.v-leave-to {
-  transform: translate(100px, 0);
-  opacity: 0;
-}
-.v-leave-active {
-  transition: all .5s 0s ease;
-}
-
 </style>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  components: { Navbar }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/skill">Skill</router-link>
+        <transition mode="out-in">
+        <router-view/>
+        </transition>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+.v-enter {
+  transform: translate(-100px, 0);
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
+}
+.v-enter-active {
+  transition: all 1s 0s ease;
+}
+.v-leave {
+  transform: translate(0, 0);
+  opacity: 1;
+}
+.v-leave-to {
+  transform: translate(100px, 0);
+  opacity: 0;
+}
+.v-leave-active {
+  transition: all .5s 0s ease;
+}
+
+</style>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
