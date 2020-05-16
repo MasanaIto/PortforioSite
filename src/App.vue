1c85acffce30563e625f7e8bf3d9ecef87ea84a3
MasanaIto <z3heng0326@gmail.com>
2020-05-16 23:09:41 +0900

    ページ遷移の際にフェードアウトアクションを追加する

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <transition mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 <style lang="scss">
@@ -28,4 +30,27 @@
     }
   }
 }
+
+.v-enter {
+  transform: translate(-100px, 0);
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
+}
+.v-enter-active {
+  transition: all 1s 0s ease;
+}
+.v-leave {
+  transform: translate(0, 0);
+  opacity: 1;
+}
+.v-leave-to {
+  transform: translate(100px, 0);
+  opacity: 0;
+}
+.v-leave-active {
+  transition: all .5s 0s ease;
+}
+
 </style>
